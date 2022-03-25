5cdbed5cc7f6117484b7d409e19debefb0cc36e7
John Trammell <John.Trammell@target.com>
2022-03-24 20:11:39 -0500

    add REST resolver adapter
    Signed-off-by: John Trammell <John.Trammell@target.com>

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# pyvrs
+# PyVRS
 
-Proof of concept VRS resolver library implemented in Python3.
+PyVRS is VRS resolver library, similar to a DNS resolver, implemented in Python3.
+
+## Usage
+
+
+
+### As a library
+
+
+
+
+### Command-line
+
+
+
+
+```ini
+# hostnames with TXT records defining VRS data
+hostname = ["ovon.directory"]
+
+# IP addresses / hostnames of VRS ReST APIs
+restapi = ["localhost:8080"]
+```
--- vrs/cli.py
+++ vrs/cli.py
@@ -1,8 +1,10 @@
 import click
+import click_config_file
 import click_log
 import logging
 from pprint import pprint
-import vrs
+from vrs.resolver import resolve
+
 
 """
 Command-line entry points for the PyVRS package.
@@ -14,10 +16,20 @@ click_log.basic_config(logger)
 
 @click.command()
 @click_log.simple_verbosity_option(logger)
-@click.option('-r', '--resolver', default='ovon.directory')
+@click.option('-H', '--hostname', default=['ovon.directory'], multiple=True)
+@click.option('-r', '--restapi', multiple=True)
 @click.argument('name')
-def vresolve(resolver, name):
+@click_config_file.configuration_option()
+def vresolve(name, hostname, restapi):
     """Resolve <name>"""
-    logger.debug(f"resolver='{resolver}' name='{name}'")
-    for record in vrs.resolve(name, resolver):
+    logger.debug(f"{hostname} / {restapi} / {name}")
+
+    # coax the config parameters into arrays
+    # TODO: turn this into a full-fledged config object
+    if isinstance(hostname, str):
+        hostname = [hostname]
+    if isinstance(restapi, str):
+        restapi = [restapi]
+
+    for record in resolve(name, dict(hostname=hostname, restapi=restapi)):
         pprint(record)
